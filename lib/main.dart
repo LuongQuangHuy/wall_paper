35a95ff1cfdc57b47cd18a7cd2d67dfc39963b7a
Lương Quang Huy <tromchobacninh@gmail.com>
2020-05-11 04:05:07 +0700

    add homework day 3

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(WallPaper());
+
+class WallPaper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            color: Color.fromARGB(255, 102, 0, 0),
+            child: Column(
+
+              children: <Widget>[
+                Container(
+                  height: 350,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Text('Chỉ có' , style: TextStyle(
+                        fontFamily: 'Charmonman',
+                        fontSize: 45,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),),
+                      Container(
+                        width: 320,
+                        height: 100,
+                        alignment: Alignment.center,
+                        decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white,
+                        ),
+                        child: Text('HÀNH ĐỘNG', style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            color: Color.fromARGB(255, 102, 0, 0),
+                            fontSize: 45,
+                            fontWeight: FontWeight.w700,
+
+                        ),),
+                      ),
+                      Text('mới tạo ra' , style: TextStyle(
+                        fontFamily: 'Charmonman',
+                        fontSize: 45,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),),
+                      Text('KẾT QUẢ' , style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 70,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.yellow,
+                      ),),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Image.asset('images/background.jpg' , height: 330, ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
